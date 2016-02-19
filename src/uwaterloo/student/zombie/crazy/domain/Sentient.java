04842a18d86152984df33d0483a8103d4517d813
d87zhang <dzhang323@hotmail.com>
2016-02-19 11:01:29 -0500

    added comments to Sentient interface

--- src/uwaterloo/student/zombie/crazy/domain/Sentient.java
+++ src/uwaterloo/student/zombie/crazy/domain/Sentient.java
@@ -1,6 +1,15 @@
 package uwaterloo.student.zombie.crazy.domain;
 
 public interface Sentient {
+	/**
+	 * update the sentient's status to represent the fact that it has made a new decision.
+	 * So the sentient's action time may (and is normally expected to) change 
+	 */
 	void makeDecision();
+	
+	/**
+	 * get the remaining time for this object's current action
+	 * @return the remaining time for this object's current action
+	 */
 	long getActionTime();
 }
