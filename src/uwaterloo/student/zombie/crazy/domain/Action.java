f9b9053763b6d56bfbaef86b41fd0f42454aec5c
d87zhang <dzhang323@hotmail.com>
2016-07-02 16:17:06 -0400

    Format code

--- src/uwaterloo/student/zombie/crazy/domain/Action.java
+++ src/uwaterloo/student/zombie/crazy/domain/Action.java
@@ -3,62 +3,72 @@ package uwaterloo.student.zombie.crazy.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Action {
-	public enum ActionType {
-	    MOVING, // moving to a new location
-	    EXPLORING, // exploring current location
-	    IDLE,
-	    ENCOUNTERED,
-	    FIGHTING,
-	    RUNNING 
-	}
-	
-	final ActionType type;
-	final long totalDurationInSecs; // how much time is required to complete the action from start to finish
-	long remainingDurationInSecs; // how much more time is needed to complete the action
-	
-	// extra data (usually that go with specific action types) that doesn't necessarily need to be populated
-	Structure dest = null;
-	List<Sentient> encounteredSentients = new ArrayList<Sentient>();
-	
-	// constants:
-	public static final int ENCOUNTER_DURATION_IN_SECS = 60;
-	
-	Action(ActionType type, long totalDurationInSecs){
-		this.type = type;
-		this.totalDurationInSecs = totalDurationInSecs;
-		this.remainingDurationInSecs = totalDurationInSecs;
-	}
-	
-	public ActionType getType() {
-		return type;
-	}
+public class Action
+{
+    public enum ActionType
+    {
+        MOVING, // moving to a new location
+        EXPLORING, // exploring current location
+        IDLE, ENCOUNTERED, FIGHTING, RUNNING
+    }
 
-	public Structure getDest() {
-		return dest;
-	}
+    final ActionType type;
+    final long totalDurationInSecs; // how much time is required to complete the
+                                    // action from start to finish
+    long remainingDurationInSecs; // how much more time is needed to complete
+                                  // the action
 
-	public void setDest(Structure dest) {
-		this.dest = dest;
-	}
+    // extra data (usually that go with specific action types) that doesn't
+    // necessarily need to be populated
+    Structure dest = null;
+    List<Sentient> encounteredSentients = new ArrayList<Sentient>();
 
-	public List<Sentient> getEncounteredSentients() {
-		return encounteredSentients;
-	}
+    // constants:
+    public static final int ENCOUNTER_DURATION_IN_SECS = 60;
 
-	public void setEncounteredSentients(List<Sentient> encounteredSentients) {
-		this.encounteredSentients = encounteredSentients;
-	}
+    Action(ActionType type, long totalDurationInSecs)
+    {
+        this.type = type;
+        this.totalDurationInSecs = totalDurationInSecs;
+        this.remainingDurationInSecs = totalDurationInSecs;
+    }
 
-	public long getRemainingDurationInSecs() {
-		return remainingDurationInSecs;
-	}
-	
-	// post-condition: remainingDurationInSecs >= 0
-	public void reduceRemainingDurationInSecs(long amountInSecs) {
-		remainingDurationInSecs -= amountInSecs;
-		if (0 > remainingDurationInSecs) {
-			remainingDurationInSecs = 0;
-		}
-	}
+    public ActionType getType()
+    {
+        return type;
+    }
+
+    public Structure getDest()
+    {
+        return dest;
+    }
+
+    public void setDest(Structure dest)
+    {
+        this.dest = dest;
+    }
+
+    public List<Sentient> getEncounteredSentients()
+    {
+        return encounteredSentients;
+    }
+
+    public void setEncounteredSentients(List<Sentient> encounteredSentients)
+    {
+        this.encounteredSentients = encounteredSentients;
+    }
+
+    public long getRemainingDurationInSecs()
+    {
+        return remainingDurationInSecs;
+    }
+
+    // post-condition: remainingDurationInSecs >= 0
+    public void reduceRemainingDurationInSecs(long amountInSecs)
+    {
+        remainingDurationInSecs -= amountInSecs;
+        if (0 > remainingDurationInSecs) {
+            remainingDurationInSecs = 0;
+        }
+    }
 }
--- src/uwaterloo/student/zombie/crazy/domain/Group.java
+++ src/uwaterloo/student/zombie/crazy/domain/Group.java
@@ -3,69 +3,80 @@ package uwaterloo.student.zombie.crazy.domain;
 import java.util.ArrayList;
 
 public class Group implements Sentient {
-	// constants:
-	private static final float ENCOUNTER_PROB = (float) 1.0; // in percentage per minute
-	
-	Action action;
-	ArrayList<Creature> members = new ArrayList<Creature>(); 
-	Structure location;
-	
-	public void addMember(Creature creature){
-		if( !members.isEmpty()){
-			if( (creature instanceof Human && members.get(0) instanceof Zombie) 
-					|| (creature instanceof Zombie && members.get(0) instanceof Human) ){
-				throw new RuntimeException("incompatible type");
-			}
-		}
-		members.add(creature);
-		
-	}
-	
-	public void removeMember(Creature creature){
-		members.remove(creature);
-	}
-	
-	@Override
-	public void makeDecision() {
-		// TODO Auto-generated method stub
-		
-	}
+    // constants:
+    private static final float ENCOUNTER_PROB = (float) 1.0; // in percentage
+                                                             // per minute
 
-	@Override
-	public Action getAction() {
-		return action;
-	}
+    Action action;
+    ArrayList<Creature> members = new ArrayList<Creature>();
+    Structure location;
 
-	@Override
-	public float getEncounterProbabiltyWith(Sentient other) {
-		return ENCOUNTER_PROB;
-	}
+    public void addMember(Creature creature)
+    {
+        if (!members.isEmpty()) {
+            if ((creature instanceof Human && members.get(0) instanceof Zombie)
+                    || (creature instanceof Zombie && members.get(0) instanceof Human)) {
+                throw new RuntimeException("incompatible type");
+            }
+        }
+        members.add(creature);
 
-	@Override
-	public void setAction(Action action) {
-		this.action = action;
-	}
+    }
 
-	@Override
-	public Structure getLocation() {
-		return location;
-	}
-
-	@Override
-	public void setLocation(Structure location) {
-		this.location = location;
-	}
+    public void removeMember(Creature creature)
+    {
+        members.remove(creature);
+    }
 
     @Override
-    public String getName() {
+    public void makeDecision()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Action getAction()
+    {
+        return action;
+    }
+
+    @Override
+    public float getEncounterProbabiltyWith(Sentient other)
+    {
+        return ENCOUNTER_PROB;
+    }
+
+    @Override
+    public void setAction(Action action)
+    {
+        this.action = action;
+    }
+
+    @Override
+    public Structure getLocation()
+    {
+        return location;
+    }
+
+    @Override
+    public void setLocation(Structure location)
+    {
+        this.location = location;
+    }
+
+    @Override
+    public String getName()
+    {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public void advanceStateForTime(int secs) {
+    public void advanceStateForTime(int secs)
+    {
         // TODO Auto-generated method stub
-        
-    } 
-	
+
+    }
+
 }
